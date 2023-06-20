dcde4748269eec57cddf4d9e952d7d377d73503a
Jayathunga T.M <thiroshmadhusha0520@gmail.com>
2023-06-21 01:28:07 +0530

    Create header section and add images

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import {Container, Row, Button} from 'reactstrap';  
+import {Link, NavLink} from 'react-router-dom';
+
+import Logo from '../../assets/images/logo.png';
+import "./header.css";
+
+const nav__links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Container>
+        <Row>
+          <div className="nav__wrapper d-flex align-items-center justify-content-between">
+            {/* Logo */}
+            <div className="logo">
+              <img src={Logo} alt="logo" />
+            </div>
+
+            {/* End Logo */}
+            {/* Menu Start */}
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {nav__links.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "active__link" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* End Menu */}
+
+            {/* <div className="nav__right d-flex align-items-center gap-4">
+              <div className="nav__btns d-flex align-items-center gap-4">
+                <Button className="btn secondary__btn">
+                  <Link to="/login">Login</Link>
+                </Button>
+                <Button className="btn primary__btn">
+                  <Link to="/register">Register</Link>
+                </Button>
+                <Button className="btn primary__btn">
+                  <Link to="/register">Register</Link>
+                </Button>
+              </div>
+              <span className="mobile__menu">
+                <i class="ri-menu-line"></i>
+              </span>
+            </div> */}
+            
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
